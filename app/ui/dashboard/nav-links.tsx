eb2cf7a3fd0c45e04ee6c5cff93f73ff8b40f93a
jerry <jerrylovee2@gmail.com>
2024-07-09 11:30:07 +0530

    Test commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -19,6 +19,7 @@ const links = [
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
+
 export default function NavLinks() {
   const pathname = usePathname();
   return (
